ffeeba8d1e2a655907031f55898f6d8f65a49627
Antonimason <elchuli3000@gmail.com>
2023-10-06 14:40:17 +0100

    fixing deploy

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from 'react';
 import YourButton from './Layouts/YourButton'
 import BuildButton from './Layouts/BuildButton';
 import Clipboard from './Components/Clipboard/Clipboard';
-import Frame from './Assets/frame.jpg'
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { display } from './Redux/CrazyButton';
-import { clipboards } from './Redux/buttonSelection';
 
 function App() {
 
